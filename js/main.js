e2a366b59dbc26908d4785dc6ca77a3af174401e
jordan@server.fake <jordan@upstart-Latitude-2110.(none)>
2014-05-01 17:39:06 -0400

    code

--- js/main.js
+++ js/main.js
@@ -19,8 +19,24 @@ $(".slidesjs-next").text(">");
     Tabletop.init( { key: key, callback: showinfo, parseNumbers: true } );
     function showinfo(data) {
       console.log(data)
-    }
-  }
- 
+      var fiftyfiveOptions = {
+        "data" : data["55m Dash"].all(),
+        "pagination": 10,
+        "tableDiv": "#fiftyfive",
+        "filterDiv":"#fiftyfiveFilter"
+        };
+        Sheetsee.makeTable(fiftyfiveOptions);
+        Sheetsee.initiateTableFilter(fiftyfiveOptions);
+         var threehundredOptions = {
+        "data" : data["300m dash"].all(),
+        "pagination": 10,
+        "tableDiv": "#threehundred",
+        "filterDiv":"#threehundredFilter"
+        };
+        Sheetsee.makeTable(threehundredOptions);
+        Sheetsee.initiateTableFilter(threehundredOptions);
+        
 
+        }
+      }
 });	
