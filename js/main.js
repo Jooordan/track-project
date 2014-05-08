c2cd337d9fb3359ba4418d46df377cf9f8a2a3b7
jordan@server.fake <jordan@upstart-Latitude-2110.(none)>
2014-05-08 17:26:18 -0400

    tables

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ $(".slidesjs-next").text(">");
         };
         Sheetsee.makeTable(fiftyfiveOptions);
         Sheetsee.initiateTableFilter(fiftyfiveOptions);
+
          var threehundredOptions = {
         "data" : data["300m dash"].all(),
         "pagination": 10,
@@ -36,6 +37,80 @@ $(".slidesjs-next").text(">");
         Sheetsee.makeTable(threehundredOptions);
         Sheetsee.initiateTableFilter(threehundredOptions);
         
+         var sixhundredOptions = {
+        "data" : data["600m run"].all(),
+        "pagination": 10,
+        "tableDiv": "#sixhundred",
+        "filterDiv":"#sixhundredFilter"
+        };
+        Sheetsee.makeTable(threehundredOptions);
+        Sheetsee.initiateTableFilter(threehundredOptions);
+        
+        var onethousandOptions = {
+        "data" : data["1000m run"].all(),
+        "pagination": 10,
+        "tableDiv": "#onethousand",
+        "filterDiv":"#onethousandFilter"
+        };
+        Sheetsee.makeTable(sixhundredOptions);
+        Sheetsee.initiateTableFilter(sixhundredOptions);
+        
+        var onethousandOptions = {
+        "data" : data["1000m run"].all(),
+        "pagination": 10,
+        "tableDiv": "#onethousand",
+        "filterDiv":"#onethousandFilter"
+        };
+        Sheetsee.makeTable(onethousandOptions);
+        Sheetsee.initiateTableFilter(onethousandOptions);
+       
+       var onethousandsixhundredOptions = {
+        "data" : data["1600m sp"].all(),
+        "pagination": 10,
+        "tableDiv": "#onethousandsixhundred",
+        "filterDiv":"#onethousandsixhundredFilter"
+        };
+        Sheetsee.makeTable(onethousandsixhundredOptions);
+        Sheetsee.initiateTableFilter(onethousandsixhundredOptions);
+        
+         var onemileOptions = {
+        "data" : data["1 mile"].all(),
+        "pagination": 10,
+        "tableDiv": "#onemile",
+        "filterDiv":"#onemileFilter"
+        };
+        Sheetsee.makeTable(onemileOptions);
+        Sheetsee.initiateTableFilter(onemileOptions);
+
+        var fourbytwohundredrelayOptions = {
+        "data" : data["4x200m relay"].all(),
+        "pagination": 10,
+        "tableDiv": "#fourbytwohundredrelay",
+        "filterDiv":"#fourbytwohundredFilter"
+        };
+        Sheetsee.makeTable(fourbytwohundredrelayOptions);
+        Sheetsee.initiateTableFilter(fourbytwohundredrelayOptions);
+  
+
+        var fourbyfourhundredrelayOptions = {
+        "data" : data["4x4m relay"].all(),
+        "pagination": 10,
+        "tableDiv": "#fourbyfourhundredrelay",
+        "filterDiv":"#fourbyfourhundredFilter"
+        };
+        Sheetsee.makeTable(fourbyfourhundredrelayOptions);
+        Sheetsee.initiateTableFilter(fourbyfourhundredrelayOptions);
+        
+       
+        
+        
+
+        
+        
+        
+
+        
+
 
         }
       }
